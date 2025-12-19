0f65566a51e09a5462e30e50873cd57889f24e3d
SnowE <110741768+SnnowE@users.noreply.github.com>
2025-12-19 16:18:17 +0100

    R/W-Driver

---
+++ Driver/Driver/main.cpp
@@ -0,0 +1,16 @@
+#include <ntddk.h>
+
+VOID DriverUnload(DRIVER_OBJECT* DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	DbgPrint("bye");
+}
+
+extern "C" NTSTATUS
+DriverEntry(DRIVER_OBJECT* DriverObject, PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(RegistryPath);
+	DbgPrint("Hello World");
+	DriverObject->DriverUnload = DriverUnload;
+	return STATUS_SUCCESS;
+}
