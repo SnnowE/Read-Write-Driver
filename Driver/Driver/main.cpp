91fe4c7d9fc3ba5f79f80fa250803ec9116db1d1
SnowE <110741768+SnnowE@users.noreply.github.com>
2025-12-19 20:38:56 +0100

    update

--- Driver/Driver/main.cpp
+++ Driver/Driver/main.cpp
@@ -1,16 +1,22 @@
 #include <ntddk.h>
 
-VOID DriverUnload(DRIVER_OBJECT* DriverObject)
-{
+VOID DriverUnload(DRIVER_OBJECT* DriverObject) {
+
 	UNREFERENCED_PARAMETER(DriverObject);
 	DbgPrint("bye");
 }
 
 extern "C" NTSTATUS
-DriverEntry(DRIVER_OBJECT* DriverObject, PUNICODE_STRING RegistryPath)
-{
+DriverEntry(DRIVER_OBJECT* DriverObject, PUNICODE_STRING RegistryPath){
+
 	UNREFERENCED_PARAMETER(RegistryPath);
 	DbgPrint("Hello World");
 	DriverObject->DriverUnload = DriverUnload;
 	return STATUS_SUCCESS;
+}
+
+extern "C" NTSTATUS
+Driver_Dispatch(PDEVICE_OBJECT DriverObject, PIRP Irp) {
+	
+
 }
