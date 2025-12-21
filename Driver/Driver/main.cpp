da263bfe2a98685ad3ce052211c15f1a5e163694
SnowE <110741768+SnnowE@users.noreply.github.com>
2025-12-21 04:34:24 +0100

    update

--- Driver/Driver/main.cpp
+++ Driver/Driver/main.cpp
@@ -1,10 +1,16 @@
 #include <ntddk.h>
 
+PDEVICE_OBJECT DeviceObject;
+
 VOID DriverUnload(DRIVER_OBJECT* DriverObject) {
 
 	UNREFERENCED_PARAMETER(DriverObject);
 	DbgPrint("bye");
 }
+
+NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DriverObject, PIRP Irp);
+
+NTSTATUS BoosterWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp);
 
 extern "C" NTSTATUS
 DriverEntry(DRIVER_OBJECT* DriverObject, PUNICODE_STRING RegistryPath){
@@ -15,18 +21,16 @@ DriverEntry(DRIVER_OBJECT* DriverObject, PUNICODE_STRING RegistryPath){
 	
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = BoosterCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = BoosterCreateClose;
-	
 	DriverObject->MajorFunction[IRP_MJ_WRITE] = BoosterWrite;
 
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Booster");
+	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create divice object *ox%o8X)\n", status));
+		return status;
+	}
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DriverObject, PIRP Irp) {
-	
-
-}
-
-NTSTATUS BoosterWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
 
 
-}
