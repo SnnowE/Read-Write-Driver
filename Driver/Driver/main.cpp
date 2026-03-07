a5ca89344df2eaf5061b9302b66efe6286a5ed43
SnowE <110741768+SnnowE@users.noreply.github.com>
2026-03-07 13:19:17 +0100

    update

--- Driver/Driver/main.cpp
+++ Driver/Driver/main.cpp
@@ -1,11 +1,7 @@
-#include <ntddk.h>
+#include <ntifs.h>
+#include "BoosterCommon.h"
 
 PDEVICE_OBJECT DeviceObject;
-
-void SampleUnload(PDRIVER_OBJECT DriverObject) {
-	UNREFERENCED_PARAMETER(DriverObject);
-	KdPrint(("Sample driver Unlad called\n"));
-}
 
 void BoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
 	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Booster");
@@ -14,8 +10,8 @@ void BoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
 	IoDeleteDevice(DriverObject->DeviceObject);
 }
 
-NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DriverObject, PIRP Irp) {
-	UNREFERENCED_PARAMETER(DeviceObject);
+NTSTATUS BoosterCreateClose(PDEVICE_OBJECT deviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(deviceObject);
 
 	Irp->IoStatus.Status = STATUS_SUCCESS;
 	Irp->IoStatus.Information = 0;
@@ -23,19 +19,47 @@ NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DriverObject, PIRP Irp) {
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS BoosterWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+NTSTATUS BoosterWrite(PDEVICE_OBJECT deviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(deviceObject);
 	auto status = STATUS_SUCCESS;
 	ULONG_PTR information = 0;
 
 	auto irpSp = IoGetCurrentIrpStackLocation(Irp);
+
+	do {
+		if (irpSp->Parameters.Write.Length < sizeof(ThreadData)) {
+			status = STATUS_BUFFER_TOO_SMALL;
+			break;
+		}
+		auto data = static_cast<ThreadData*>(Irp->UserBuffer);
+		if (data == nullptr || data->Priority < 1 || data->Priority > 31) {
+			status = STATUS_INVALID_PARAMETER;
+			break;
+		}
+
+		PETHREAD thread;
+		status = PsLookupThreadByThreadId(ULongToHandle(data->ThreadId), &thread);
+		if (!NT_SUCCESS(status))
+			break;
+
+		auto oldPriority = KeSetPriorityThread(thread, data->Priority);
+		KdPrint(("Priority change for thread %u from %d to %d succeeded!\n", data->ThreadId, oldPriority, data->Priority));
+		ObDereferenceObject(thread);
+
+		information = sizeof(ThreadData);
+
+	} while (false);
+
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = information;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return status;
 }
 
 extern "C" NTSTATUS
 DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath){
-
 	UNREFERENCED_PARAMETER(RegistryPath);
 	KdPrint(("Hello World\n"));
-	DriverObject->DriverUnload = SampleUnload;
 	DriverObject->DriverUnload = BoosterUnload;
 
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = BoosterCreateClose;
@@ -45,20 +69,18 @@ DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath){
 	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Booster");
 	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
 	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create divice object *ox%o8X)\n", status));
+		KdPrint(("Failed to create device object (0x%08X)\n", status));
 		return status;
 	}
+	DeviceObject->Flags |= DO_BUFFERED_IO;
 
 	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Booster");
 	status = IoCreateSymbolicLink(&symLink, &devName);
 	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create symbolic link (0x%08X\n", status));
+		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
 		IoDeleteDevice(DeviceObject);
 		return status;
 	}
 
 	return STATUS_SUCCESS;
 }
-
-
-
